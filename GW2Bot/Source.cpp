a5495cb9b23f4aaf934170b47c46f4cb16441879
Z0F <28806618+0xZ0F@users.noreply.github.com>
2021-11-22 15:34:31 -0500

    Added Multiboxing Support

--- GW2Bot/Source.cpp
+++ GW2Bot/Source.cpp
@@ -4,7 +4,7 @@ AFK Farming bot for GW2 engineer class. Summons turrets automatically. Better to
 Keybinds are set in Keybinds.h.
 There are 2 types of turrets used. Two have 20 second cooldowns upon expiration (5 minutes), the other 2 have 40 second cooldowns.
 Two threads actively manage 2 turrets each because of the different cooldowns. However, each thread uses the same function.
-
+Supports multiboxing.
 */
 
 #include <Windows.h>
@@ -13,12 +13,15 @@ Two threads actively manage 2 turrets each because of the different cooldowns. H
 #include <chrono>
 #include <thread>
 #include <future>
+#include <vector>
 #include "Log.h"
 #include "Keybinds.h"
 
-std::unique_ptr<std::thread> WorkerTwentySeconds = std::make_unique<std::thread>();
-std::unique_ptr<std::thread> WorkerFourtySeconds = std::make_unique<std::thread>();
-bool IsWorkerTwentySeconds = false;
+static LPCSTR g_WindowName = "Guild Wars 2";
+static std::unique_ptr<std::thread> g_WorkerTwentySeconds = std::make_unique<std::thread>();
+static std::unique_ptr<std::thread> g_WorkerFourtySeconds = std::make_unique<std::thread>();
+static bool g_IsWorkerTwentySeconds = false;
+static std::vector<HWND> g_WindowHandles;
 
 void SendKey(SHORT key) {
 	UINT mappedkey;
@@ -36,99 +39,77 @@ void SendKey(SHORT key) {
 	SendInput(1, &input, sizeof(input));
 }
 
-void PickupTurrets() {
-	HWND hWindow = FindWindow(NULL, L"Guild Wars 2");
-	HWND hOriginalWindow = GetForegroundWindow();
-
-	if (!hWindow) {
-		Log("Failed to find window.");
-		return;
+BOOL CALLBACK EnumWindowTitles(HWND hwnd, LPARAM lParam) {
+	char str[1024] = { 0 };
+	GetWindowTextA(hwnd, str, 1024);
+	if (strncmp(str, g_WindowName, 1024) == 0) {
+		g_WindowHandles.push_back(hwnd);
 	}
-
-	Log("Picking up turrets.");
-
-	SetForegroundWindow(hWindow);
-	Sleep(150);
-
-	SendKey(VK_OEM_6);
-	Sleep(500);
-	SendKey(VK_OEM_6);
-	Sleep(500);
-	SendKey(VK_OEM_6);
-	Sleep(500);
-	SendKey(VK_OEM_6);
-	Sleep(500);
-
-	if (hOriginalWindow) {
-		SetForegroundWindow(hOriginalWindow);
-	}
+	return true;
 }
 
 // Every five minutes + n seconds place turrets specified in the array:
 void Worker(int additionalSeconds, int turrets[], int size) {
-	HWND hWindow = FindWindow(NULL, L"Guild Wars 2");
 	HWND hOriginalWindow = GetForegroundWindow();
-
-	if (!hWindow) {
-		Log("Failed to find window.");
-		return;
-	}
 
 	Log("Worker ", std::this_thread::get_id(), " started.");
 
 	do {
 		// Prevent the two threads from placing turrets at the same time:
-		if (WorkerTwentySeconds->get_id() == std::this_thread::get_id()) {
-			IsWorkerTwentySeconds = true;
+		if (g_WorkerTwentySeconds->get_id() == std::this_thread::get_id()) {
+			g_IsWorkerTwentySeconds = true;
 		}else {
-			while (IsWorkerTwentySeconds) {
+			while (g_IsWorkerTwentySeconds) {
 				Sleep(100);
 			}
 		}
 
-		Log("Worker ", std::this_thread::get_id(), " placing turrets (delayed by ", additionalSeconds, " seconds).");
+		for (unsigned short x = 0; x < g_WindowHandles.size(); x++) {
+			Log("Worker ", std::this_thread::get_id(), " placing turrets (delayed by ", additionalSeconds, " seconds) for ", g_WindowHandles[x]);
 
-		SetForegroundWindow(hWindow);
-		Sleep(250);
+			SetForegroundWindow(g_WindowHandles[x]);
+			Sleep(200);
 
-		for (size_t x = 0; x < size; x++) {
-			SendKey(turrets[x]);
-			Sleep(1000);
+			for (size_t x = 0; x < size; x++) {
+				SendKey(turrets[x]);
+				Sleep(1000);
+			}
 		}
-
-		Sleep(150);
 
 		if (hOriginalWindow) { SetForegroundWindow(hOriginalWindow); }
 
 		Log("Worker ", std::this_thread::get_id(), " sleeping for 5 minutes ", additionalSeconds, " seconds.");
-		IsWorkerTwentySeconds = false;
-		std::this_thread::sleep_for(std::chrono::minutes(5) + std::chrono::seconds(additionalSeconds));
+		g_IsWorkerTwentySeconds = false;
+		std::this_thread::sleep_for(std::chrono::minutes(5) - std::chrono::seconds(size) + std::chrono::seconds(additionalSeconds));
 	} while (true);
 }
 
 int main() {
 	Log("F8 - START");
 	Log("F9 - STOP");
-	Log("F10 - Pickup turrets.");
 	int TwentySecondTurrets[2] = {0};
 	int FourtySecondTurrets[2] = {0};
 
 	do {
 		if (GetAsyncKeyState(VK_F8) & 1) {
-			if (!WorkerTwentySeconds->native_handle()) {
+			EnumWindows(EnumWindowTitles, NULL);
+			if (g_WindowHandles.size() < 1) {
+				Log("Failed to find a window.");
+				g_WindowHandles.clear();
+				continue;
+			}
+
+			if (!g_WorkerTwentySeconds->native_handle()) {
 				TwentySecondTurrets[0] = HEALING_TURRET;
 				TwentySecondTurrets[1] = RIFLE_TURRET;
-				*WorkerTwentySeconds = std::thread(Worker, 20, TwentySecondTurrets, 2);
+				*g_WorkerTwentySeconds = std::thread(Worker, 20, TwentySecondTurrets, 2);
 				Sleep(100);
 			}
-			if (!WorkerFourtySeconds->native_handle()) {
+			if (!g_WorkerFourtySeconds->native_handle()) {
 				FourtySecondTurrets[0] = ROCKET_TURRET;
 				FourtySecondTurrets[1] = THUMPER_TURRET;
-				*WorkerFourtySeconds = std::thread(Worker, 40, FourtySecondTurrets, 2);
+				*g_WorkerFourtySeconds = std::thread(Worker, 40, FourtySecondTurrets, 2);
 			}				
-		}		
-		else if (GetAsyncKeyState(VK_F10) & 1) {
-			PickupTurrets();
 		}
 		std::this_thread::sleep_for(std::chrono::milliseconds(100));
 	} while (!GetAsyncKeyState(VK_F9));
